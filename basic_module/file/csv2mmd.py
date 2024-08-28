d2b73cb36adb3bd50a1b9d51c43606029e0de4b6
sundafei <sundafei@sniper-tech.com>
2024-08-28 16:23:50 +0800

    add lost change

--- basic_module/file/csv2mmd.py
+++ basic_module/file/csv2mmd.py
@@ -5,21 +5,26 @@ from tree import TreeNode
 
 templ_repl = "，。？、：（）%"
 
-root = TreeNode("")
+
+def readRawCsv(file_name):
+    lines = []
+    with open(file_name, "r") as f:
+        for line in f:
+            line = line.strip()
+            items = line.split(',')
+            lines.append(items)
+    return lines
 
 
-def traverseNode(node, fw):
-    for child in node.children:
-        line = f"{node.value}-->{child.value}"
-        fw.write(line)
-        fw.write("\n")
-        traverseNode(child, fw)
-
-
-def traverseRoot(fw):
-    if (0 == len(root.children)):
-        return
-    traverseNode(root.children[0], fw)
+def removeDupData(lines):
+    for i in range(len(lines)-1, 0, -1):
+        cur_line = lines[i]
+        last_line = lines[i-1]
+        for j in range(1, len(cur_line)):
+            if len(last_line) <= j:
+                break
+            if cur_line[j] == last_line[j]:
+                cur_line[j-1] = ""
 
 
 def specialCharRep(_string):
@@ -30,45 +35,27 @@ def specialCharRep(_string):
     return ret
 
 
-def formatData(line):
-    n = root
-    # print(line)
-    for i in range(len(line)):
+# if (len(sys.argv) < 2):
+#     print("input file name error")
+#     sys.exit()
+# if (len(sys.argv) < 3):
+#     print("output file name error")
+#     sys.exit()
+# file_input = sys.argv[1]
+file_input = "thunder10.csv"
+# file_output = sys.argv[2]
+file_output = "thunder10_2.csv"
 
-        if not (n.containsChild(line[i])):
-            n.add_child(TreeNode(line[i]))
-        n = n.children[n.indexOfValue(line[i])]
-        pass
-
-
-if (len(sys.argv) < 2):
-    print("input file name error")
-    sys.exit()
-if (len(sys.argv) < 3):
-    print("output file name error")
-    sys.exit()
-
-file_input = sys.argv[1]
-file_output = sys.argv[2]
-
-print(file_input)
-print(file_output)
-
+lines = readRawCsv(file_input)
+removeDupData(lines)
 
 fw = open(file_output, "w")
-fw.write("flowchart LR\n")
-
-with open(file_input, "r") as f:
-    for line in f:
-        line = line.strip()
-        items = line.split(',')
-        for i in range(len(items)):
-            items[i] = specialCharRep(items[i])
-
-        # formatData(items)
-        new_line = "-->".join(items)
-        fw.write(new_line)
-        fw.write("\n")
-    # traverseRoot(fw)
+# fw.write("flowchart LR\n")
+for line in lines:
+    # filtered_line = list(filter(None, line))
+    new_line = ",".join(line)
+    # new_line = specialCharRep(new_line)
+    fw.write(new_line)
+    fw.write("\n")
 
 fw.close()
