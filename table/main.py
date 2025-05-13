524f22376c13daac2d68a91eb5cc6ac032ad1c07
bear08 <957011893@qq.com>
2025-05-13 19:08:58 +0800

    update table

--- table/main.py
+++ table/main.py
@@ -4,6 +4,14 @@ import pandas as pd
 cg = Config("config/config.json")
 # cg.output()
 
-df = pd.read_excel("test.xlsx", sheet_name=None)
+df = pd.read_excel("test.xlsx", sheet_name="Sheet2")
 
-print(df)
+# print(df.info())
+# print(df.describe())
+# print(df.dtypes)
+
+columns = df.columns
+
+df2 = df.sort_values()
+# print(df)
+print(df2)
